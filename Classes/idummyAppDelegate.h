51d853f534db3956d63a032309a22ce7a62fe330
Riley Andrews <riley.andrews@gmail.com>
2010-11-07 17:07:19 -0800

    first commit

---
+++ Classes/idummyAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  idummyAppDelegate.h
+//  idummy
+//
+//  Created by apple on 11/7/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface idummyAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
